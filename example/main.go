df199a640dec8cad7594eab64362593193e734e4
reus <reusee@gmail.com>
2015-01-17 12:04:45 +0800

    example refinements

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import (
 	"container/list"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/BurntSushi/xgb/xproto"
@@ -17,8 +18,16 @@ var (
 func main() {
 	var wm *wmutil.Wm
 	windows := list.New()
-	mod := uint16(xproto.ModMaskControl)
+	kill := make(chan struct{})
+	mod := uint16(xproto.ModMask4)
+	display := os.Getenv("DISPLAY")
+	if display == ":2" {
+		mod = uint16(xproto.ModMaskControl)
+	}
 	keyBindings := map[wmutil.Stroke]func(){
+		wmutil.Stroke{mod, wmutil.Key_q}: func() {
+			close(kill)
+		},
 		wmutil.Stroke{mod, wmutil.Key_Return}: func() {
 			exec.Command("sakura").Start()
 		},
@@ -32,10 +41,18 @@ func main() {
 			}
 		},
 		wmutil.Stroke{mod, wmutil.Key_j}: func() {
+			if windows.Len() <= 1 {
+				return
+			}
 			front := windows.Remove(windows.Front()).(*wmutil.Window)
 			back := windows.Back().Value.(*wmutil.Window)
 			front.Below(back)
 			windows.PushBack(front)
+			next := windows.Front().Value.(*wmutil.Window)
+			if wm.PointingWindow() != next {
+				next.WarpPointer()
+			}
+			wm.FocusPointerRoot()
 		},
 	}
 
@@ -58,14 +75,24 @@ func main() {
 		select {
 		case win := <-wm.Map:
 			win.SetGeometry(0, 0, screenWidth, screenHeight)
-			win.WarpPointer()
+			if wm.PointingWindow() != win {
+				win.WarpPointer()
+			}
 			windows.PushFront(win)
+			wm.FocusPointerRoot()
 		case win := <-wm.Unmap:
-			_ = win
+			for e := windows.Front(); e != nil; e = e.Next() {
+				if e.Value == win {
+					windows.Remove(e)
+					break
+				}
+			}
 		case stroke := <-wm.Stroke:
 			if cb, ok := keyBindings[stroke]; ok {
 				cb()
 			}
+		case <-kill:
+			return
 		}
 	}
 }
